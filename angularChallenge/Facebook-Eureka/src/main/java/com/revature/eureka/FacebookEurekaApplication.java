6b9ad0e30fd0c8590a25f37d54f0905ca933db87
Manuel Tenorio <tenoriom54@yahoo.com>
2017-11-09 10:22:29 -0500

    Inital project set up

---
+++ angularChallenge/Facebook-Eureka/src/main/java/com/revature/eureka/FacebookEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.revature.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookEurekaApplication.class, args);
+	}
+}
---
+++ angularChallenge/Facebook-Zuul/src/main/java/com/revature/facebook/zuul/FacebookZuulApplication.java
@@ -0,0 +1,12 @@
+package com.revature.facebook.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookZuulApplication.class, args);
+	}
+}
