0a1b13a4ee8ea92a06940cb4e9cdacb2dd7fcf83
Brandon <bdf1006@aol.com>
2017-11-10 14:16:01 -0500

    Config beginning

---
+++ angularChallenge/Facebook-Config-Server/src/main/java/com/revature/config/FacebookConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.revature.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookConfigServerApplication.class, args);
+	}
+}
