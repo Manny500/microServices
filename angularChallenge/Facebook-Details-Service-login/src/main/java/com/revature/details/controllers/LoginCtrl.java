942ba46bbf7d7424c0b775e8784b417a82b956d0
Manuel Tenorio <tenoriom54@yahoo.com>
2017-11-11 20:30:05 -0500

    angular setup

--- angularChallenge/Facebook-Details-Service-login/src/main/java/com/revature/details/controllers/LoginCtrl.java
+++ angularChallenge/Facebook-Details-Service-login/src/main/java/com/revature/details/controllers/LoginCtrl.java
@@ -1,17 +1,37 @@
 package com.revature.details.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.revature.details.beans.Customer;
+import com.revature.details.beans.FaceUser;
+import com.revature.details.repo.FaceUserRepo;
 
 @EnableEurekaClient
 @Controller
 public class LoginCtrl {
+	
+	@Autowired
+	FaceUserRepo userRepo;
+	
+	private final static String POST_CUSTOMER_URL = "/login";
 
-	@RequestMapping("/")
-	public String loginPage(){
-		System.out.println("LoginPage - GET");
-		return "forward:login.html";
+	@PostMapping(POST_CUSTOMER_URL)
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+		
+		System.out.println("Creat Customer: " + customer);
+		
+		FaceUser user = new FaceUser();
+		
+		user.setFaceUsername("j");
+		
+		System.out.println("From database: "+ userRepo.findByfaceUsername(user.getFaceUsername()).getFaceFn());
+		
+		return ResponseEntity.ok(customer);
 	}
 
 }
--- angularChallenge/Facebook-Details-Service-login/src/main/java/com/revature/service/PerformLoginService.java
+++ angularChallenge/Facebook-Details-Service-login/src/main/java/com/revature/service/PerformLoginService.java
@@ -3,6 +3,7 @@ package com.revature.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.details.beans.FaceUser;
 import com.revature.details.repo.FaceUserRepo;
 
 @Service(value = "userDetailsService")
@@ -10,5 +11,10 @@ public class PerformLoginService {
 
 	@Autowired
 	FaceUserRepo userRepo;
+	
+	public FaceUser authenticate(FaceUser user) {
+		
+		return userRepo.findByfaceUsername(user.getFaceUsername());
+	}
 
 }
